e5f77cb9d20ef9b27fadf85255fa1be4130ac4b0
AMIYA8597 <chowdhuryamiya41@gmail.com>
2024-02-13 15:49:04 +0530

    1st commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// const express = require('express')
+// const app = express();
+
+// app.get('/', function (req, res) {
+//   res.send('Hello, i am saterting new mvc project')
+// })
+ 
+//  app.listen(5002, (req, res) => {
+//     console.log("server is running on port 5002");
+//   });
