5d65308b7c707fc07cdc111ad505205c34347d37
AMIYA8597 <chowdhuryamiya41@gmail.com>
2024-02-26 19:53:08 +0530

    add-backend and frontend

---
+++ frontend/my-react/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import BasicCard from "./components/Card"
+function App() {
+  return (
+   <>
+   <BasicCard/>
+   </>
+  );
+}
+
+export default App;
