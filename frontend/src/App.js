b6ae850f0db88507c94f685a1d200f0b76a7c0e6
AMIYA8597 <chowdhuryamiya41@gmail.com>
2024-02-27 19:24:28 +0530

    adding

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-// import MultiActionAreaCard from "./components/Card"
-import ResponsiveAppBar from "./components/ResponsiveApp"
+
 function App() {
   return (
-   <>
-   {/* <MultiActionAreaCard/> */}
-   <ResponsiveAppBar/>
-   </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
