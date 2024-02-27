259fcc3dd5044b553ae422f7215fb2825e6140b6
AMIYA8597 <chowdhuryamiya41@gmail.com>
2024-02-27 18:07:40 +0530

    facing error

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+// import MultiActionAreaCard from "./components/Card"
+import ResponsiveAppBar from "./components/ResponsiveApp"
+function App() {
+  return (
+   <>
+   {/* <MultiActionAreaCard/> */}
+   <ResponsiveAppBar/>
+   </>
+  );
+}
+
+export default App;
