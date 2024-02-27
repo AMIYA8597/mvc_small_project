48c2c326cb287788e979db52681ec37d52b87589
AMIYA8597 <chowdhuryamiya41@gmail.com>
2024-02-27 20:03:40 +0530

    using nextjs instead of react

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Navbar from "../../components/Navbar";
+import BasicCard from "../../components/Card";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <Navbar/>
+  );
+}
